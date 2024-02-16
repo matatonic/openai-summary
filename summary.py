ec13b59b4a2f1c429b0864e72e125651a7d7668c
matatonic <matatonic-git@zhero.org>
2024-02-16 12:04:33 -0500

    improve youtube, stop using system prompt

--- summary.py
+++ summary.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python3
-
+#!/usr/bin/env python
 # Load the environment variables from the .env file
 from dotenv import load_dotenv
 load_dotenv()
@@ -16,28 +15,11 @@ try:
 except:
     pass
 
-import pysbd
-
 client = openai.OpenAI()
 
+import pysbd
+
 seg = pysbd.Segmenter(language='en', clean=True) # text is dirty, clean it up.
-
-
-parser = argparse.ArgumentParser(
-            prog='summary.py',
-            description='Summarize URLs or files, including YouTube videos via transcriptions',
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('url', help="URL or file to summarize (including YouTube videos via transcriptions)")
-parser.add_argument('-p', '--progress', action='store_true', default=False, help="Show percentage progress")
-parser.add_argument('-S', '--no_stream', action='store_true', default=False, help="Don't output text as it's created")
-parser.add_argument('-x', '--executive_summary', action='store_true', default=False, help="Include an Executive Summary")
-parser.add_argument('-X', '--executive_summary_only', action='store_true', default=False, help="Only output the Executive Summary")
-parser.add_argument('-t', '--tldr', action='store_true', default=False, help="Include a TL;DR")
-parser.add_argument('-T', '--tldr_only', action='store_true', default=False, help="Only output a TL;DR")
-parser.add_argument('-b', '--max_size', action='store', default=5000, type=int, help="The maximum size (in characters) to summarize at once.")
-
-args = parser.parse_args()
-
 
 # https://stackoverflow.com/questions/328356/extracting-text-from-html-file-using-python
 def get_url_html(url: str) -> str:
@@ -49,7 +31,6 @@ def get_url_html(url: str) -> str:
         'Connection': 'keep-alive'}
 
     return get(url, headers=headers).content
-
 
 def get_html_text(html: str) -> list:
     soup = BeautifulSoup(html, features="html.parser")
@@ -75,7 +56,7 @@ def extract_youtube_video_id(url: str) -> str | None:
         return found.group(1)
     return None
 
-
+# TODO: Use Time gaps to make natural breaks
 def get_video_transcript(video_id: str) -> list | None:
     """
     Fetch the transcript of the provided YouTube video
@@ -87,8 +68,7 @@ def get_video_transcript(video_id: str) -> list | None:
         # The video doesn't have a transcript
         return None
     
-    return [line["text"] for line in transcript]
-
+    return seg.segment(' '.join([line["text"] for line in transcript]))
 
 def get_url_text(url: str) -> list:
     text_list = [ f"No text found for this url: {url}" ]
@@ -131,28 +111,32 @@ def get_file_text(filename: str) -> list:
 #    return text_list
 
 
-def openai_edit(instruction: str, text: str) -> str:
+def openai_edit(instruction: str, text: str, max_tokens: int = 2048) -> str:
     # edit is deprecated, fake it.
+    # Fake system message also
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
-        messages=[{'role': 'system', 'content': instruction}, {'role': 'user', 'content': text} ],
+        messages=[{'role': 'user', 'content': instruction}, {'role': 'assistant', 'content': 'OK'}, {'role': 'user', 'content': text} ],
         temperature=0.2,
+        max_tokens=max_tokens,
     )
     
     return response.choices[0].message.content
 
-def openai_completion(prompt: str, max_tokens: int = 2000) -> str:
+def openai_completion(prompt: str, max_tokens: int, stop = ['</s>']) -> str:
     response = client.completions.create(
         model="gpt-3.5-turbo",
         prompt=prompt,
         max_tokens=max_tokens,
         temperature=0.2,
+        stop=stop,
     )
     #print(response.choices[0])
     return response.choices[0].text
 
-# arbitrary cut, should fit in 2k context with instructions
-def summarize_large_text(text_list: list, max_size = args.max_size, stream = False, progress = False) -> str:
+# organize text into paragraphs or logical chunks of a max size
+# trying for fairly uniform sized chunks
+def chunk_large_text(text_list: list, max_size: int) -> list[str]:
     txts = [] # chunks of near <= max_size
     para = '' # buffer
 
@@ -188,6 +172,12 @@ def summarize_large_text(text_list: list, max_size = args.max_size, stream = Fal
     if para:
         txts.append(para)
 
+    return txts
+
+def summarize_large_text(text_list: list, max_size: int, stream = False, progress = False) -> str:
+
+    txts = chunk_large_text(text_list, max_size)
+
     summaries = []
     txts_len = len(txts)
 
@@ -207,14 +197,34 @@ def summarize_large_text(text_list: list, max_size = args.max_size, stream = Fal
 
     return '\n'.join(summaries)
 
+def parse_args(arguments):
+    parser = argparse.ArgumentParser(
+                prog='summary.py',
+                description='Summarize URLs or files, including YouTube videos via transcriptions',
+                formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('url', help="URL or file to summarize (including YouTube videos via transcriptions)")
+    parser.add_argument('-p', '--progress', action='store_true', default=False, help="Show percentage progress")
+    parser.add_argument('-S', '--no_stream', action='store_true', default=False, help="Don't output text as it's created")
+    parser.add_argument('-x', '--executive_summary', action='store_true', default=False, help="Include an Executive Summary")
+    parser.add_argument('-X', '--executive_summary_only', action='store_true', default=False, help="Only output the Executive Summary")
+    parser.add_argument('-t', '--tldr', action='store_true', default=False, help="Include a TL;DR")
+    parser.add_argument('-T', '--tldr_only', action='store_true', default=False, help="Only output a TL;DR")
+    parser.add_argument('-b', '--max_size', action='store', default=5000, type=int, help="The maximum size (in characters) to summarize at once.")
 
-if __name__ == '__main__':
+    return parser.parse_args(arguments)
+
+def main():
+    args = parse_args(sys.argv[1:])
+    #args = parse_args(['https://www.youtube.com/watch?v=3yHWM8TG-nU', '-b', '10000000000', '-T', ])
+
     if args.executive_summary_only:
-        args.no_stream = True
+        args.tldr_only = False # force one or the other
+        args.tldr = False
+        args.executive_summary = True
 
     if args.tldr_only:
         args.executive_summary_only = False # force one or the other
-        args.no_stream = True
+        args.executive_summary = False
         args.tldr = True
 
     text_list = [ "No text found" ]
@@ -224,26 +234,39 @@ if __name__ == '__main__':
     else:
         text_list = get_file_text(args.url)
 
-    summary = summarize_large_text(text_list, max_size=args.max_size, stream= not args.no_stream, progress=args.progress)
+    # Summarize as bullet points
+    if not (args.executive_summary_only or args.tldr_only):
+        summary = summarize_large_text(text_list, max_size=args.max_size, stream= not args.no_stream, progress=args.progress)
+        if args.no_stream:
+            print(summary)
+    else:
+        summary = '\n'.join(text_list)
 
-    if args.no_stream and not (args.executive_summary_only or args.tldr_only):
-        print(summary)
-    
-    if args.executive_summary or args.executive_summary_only or args.tldr or args.tldr_only:
-        if not (args.executive_summary_only or args.tldr_only):
-            print('\nExecutive Summary')
-
+    # Executive/TL;DR
+    if args.executive_summary or args.tldr:
         # if summary too large, maybe break into a list and summarize again.
-        while len(summary) > args.max_size:
-            summary = summarize_large_text(summary.split('\n'), max_size=args.max_size, stream=False, progress=args.progress)
+        if len(summary) > args.max_size:
+            
+            # Don't stream intermediate results
+            stream = args.no_stream
+            args.no_stream = True
 
-        exec_summary = openai_edit("An executive summary of the following text.", summary)
-    
-        if not args.tldr_only:
+            while len(summary) > args.max_size:
+                summary = summarize_large_text(summary.split('\n'), max_size=args.max_size, stream=False, progress=args.progress)
+
+            args.no_stream = stream
+
+        if args.executive_summary:
+            if not args.executive_summary_only:
+                print('\nExecutive Summary')
+            exec_summary = openai_edit("Write an executive summary of the following text.", summary, max_tokens=args.max_size)
             print(exec_summary)
 
         if args.tldr:
-            tldr = openai_completion(exec_summary + "\n\nTL;DR\n")
+            tldr = openai_completion(summary + "\n\nTL;DR\n", stop=["\n\n", "</s>"], max_tokens=args.max_size)
             if not args.tldr_only:
                 print('\nTL;DR')
             print(tldr)
+
+if __name__ == '__main__':
+    main()
